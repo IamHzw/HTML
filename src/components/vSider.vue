31c94b62a6ed99add719f64300ccfa71246e2213
lianghaoyi <lianghaoyi@caict.ac.cn>
2019-07-09 22:59:01 +0800

    购物车和订单页

--- src/components/vSider.vue
+++ src/components/vSider.vue
@@ -22,11 +22,74 @@
                     <div class="mslide"><img src="../assets/images/ewm.png"></div>
                 </a>
             </li>
-            <li>
+            <li >
                 <a href="javascript:;">
-                    <i><img src="../assets/images/timg.png" style="heigth:200%;"></i>
-                    <p>购物车</p>
-                    <!-- <div class="mslide"><img src="../assets/images/ewm.png"></div> -->
+                    <div @click="onCart()">
+                        <i><img src="../assets/images/timg.png" style="heigth:200%;"></i>
+                        <p>购物车</p>
+                    </div>
+                    <div class="mslide" v-bind:class="{changeColor:current}">
+                        <div class="plugin-bd" style="height:625px">
+                            <div class="tm-mcListBox">
+                                <div class="tm-mcTop">
+                                    <!-- <span>查看全部</span> -->
+                                    <div class="fl">
+                                        <input type="checkbox" checked="" class="tm-mcElectCart">
+                                    </div>
+                                    <label for="tmcElectCart">全选</label>
+                                </div>
+                                <ul class="tm-list">
+                                    <li>
+                                        <div class="commName">
+                                            <div class="fl">
+                                                <input type="checkbox" checked="" class="tm-mcElectCart">
+                                            </div>
+                                            <label for="tmcElectCart">金砺旗舰店</label>
+                                            <span>￥{{pri}}</span>
+                                        </div>
+                                        <div class="commNum">
+                                            <div class="fl tm-check">
+                                                <input type="checkbox" checked="" class="tm-mcElectCart" style="width:25px;">
+                                            </div>
+                                            <div class="fl tm-img">
+                                                <img src="https://img.alicdn.com/bao/uploaded/i2/2578854143/O1CN01B83RKc1gTXQvEe2WO_!!2578854143.jpg_sum.jpg">
+                                            </div>
+                                            <div class="fl tm-txt">
+                                                黑色衣服
+                                            </div>
+
+                                            <div class="fl tm-pri">
+                                                <div class="comm-add" @click="onAdd()">+</div>
+                                                <div class="cNum fl">{{num}}</div>
+                                                <div class="comm-sub" @click="onSub()">-</div>
+                                            </div>
+
+                                            <div class="fl tm-mon">
+                                                ￥{{pri}}
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="tm-btn">
+                                <div class="tm-mcTotal clearfix">
+                                    <h3>
+                                        <span class="tm-e1">购物车</span>
+                                        <span class="tm-e2">已选</span>
+                                        <strong class="tm-mcCartNumTotal">1</strong>
+                                        <span class="tm-e2">件</span>
+                                    </h3>
+                                    <span class="tm-mcTotalFeeWrap fr">
+                                        <strong class="tm-mcTotalFee">¥{{pri*num}}</strong>
+                                    </span>
+                                </div>
+                                <div class="tm-mcCashier" data-tmc="go">
+                                    <div class="tm-mcGo">结 算<s></s>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </a>
             </li>
         </ul>
@@ -39,18 +102,146 @@
 export default {
   data () {
     return {
-      
+      current:false,
+      num:0,
+      pri:14
     }
   },
   methods: {
+    onAdd(){
+        this.num=++this.num
+    },
+    onSub(){
+        if(this.num===0){
+            return;
+        }
+        this.num=--this.num
+    },
+    onCart(){
+        this.current=!this.current
+    },
     onGoTop(){
-
-        // this.$refs.app.animate({scrollTop: 0}, 500);
-        // this.$refs.app.scrollIntoView();
         document.getElementById("app").scrollIntoView();
-
     }
   }
 }
 </script>
+
+<style scoped>
+.fl{
+    float: left;
+}
+.fr{
+    float: right;
+}
+.changeColor{
+	display: block !important;
+}
+.plugin-bd{
+    width: 272px;
+    overflow-x: hidden;
+    background-color: #e6e6e6;
+}
+.tm-mcTop{
+    height: 28px;
+}
+.tm-mcTop a{
+    height:100% !important;
+}
+
+.tm-list li{
+    height:60px;
+}
+.tm-check{
+    padding-top: 20px;
+}
+.tm-list .tm-img{
+    width: 60px;
+    height: 60px;
+}
+.tm-img img{
+    width: 100%;
+    height: 100%;
+}
+.tm-txt{
+    width: 50px;
+    padding-top: 20px;
+    overflow: hidden;
+    text-overflow:ellipsis;
+    white-space: nowrap;
+}
+.tm-pri{
+    padding-top: 20px;
+}
+.cNum{
+    margin-right: 10px;
+    margin-left: 10px
+}
+.comm-add,.comm-sub{
+    float: left;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    border: 1px solid #ccc;
+    text-align: center;
+}
+.tm-mon{
+    margin-left: 20px;
+    padding-top: 20px;
+    width: 20px;
+    text-align: center;
+}
+.tm-btn {
+    position: relative;
+    padding: 0 16px;
+    background-color: #e6e6e6;
+    z-index: 3;
+    color: #333;
+}
+
+.tm-mcTotal {
+    width: 248px;
+    height: 30px;
+    line-height: 30px;
+    background-color: #e6e6e6;
+    padding: 0;
+    color: #333;
+    overflow: hidden;
+
+}
+
+.clearfix{
+    clear: both;
+}
+
+.tm-mcTotal h3 {
+    float: left;
+    width: 100px;
+    height: 30px;
+    color: #000;
+    font-weight: 400;
+}
+.tm-mcCashier {
+    width: 248px;
+    height: 40px;
+    line-height: 40px;
+    background-color: #ff0036;
+    position: relative;
+    overflow: hidden;
+    cursor: pointer;
+}
+ .tm-mcGo {
+    position: relative;
+    margin-top: 0;
+    width: 70px;
+    margin: 0 auto;
+    height: 40px;
+    line-height: 40px;
+    text-align: left;
+    font-family: STHeiti,"microsoft yahei" simsun arial;
+    color: #fff;
+    font-size: 14px;
+    }
+</style>
+
 
